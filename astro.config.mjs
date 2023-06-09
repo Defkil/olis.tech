114bf679395206e4853bdc038d19d706abf25a18
Oliver Grüttner <gruettner@protonmail.com>
2023-06-09 03:49:50 +0200

    feat(build): astro prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [compress()],
+  integrations: [compress(), prefetch()]
 });
