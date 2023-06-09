5646bfccbad5d5f66857e29596d0112f7e77b2de
Oliver Grüttner <gruettner@protonmail.com>
2023-06-09 04:19:13 +0200

    feat(build): astro critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
-
 import rome from "astro-rome";
+
+import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://olis.tech',
   output: 'static',
-  integrations: [compress(), prefetch(), sitemap(), rome()]
+  integrations: [compress(), prefetch(), sitemap(), rome(), critters()]
 });
