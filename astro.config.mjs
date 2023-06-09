38cdd754eff0beb879bb3c4274b571c5ad145f71
Oliver Grüttner <gruettner@protonmail.com>
2023-06-10 01:58:48 +0200

    feat(build): astro solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,11 @@ import sitemap from "@astrojs/sitemap";
 import rome from "astro-rome";
 import critters from "astro-critters";
 import compressor from "astro-compressor";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://olis.tech',
   output: 'static',
-  integrations: [compress(), prefetch(), sitemap(), rome(), critters(), compressor()]
+  integrations: [compress(), prefetch(), sitemap(), rome(), critters(), compressor(), solidJs()]
 });
