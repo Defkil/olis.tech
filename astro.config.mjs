ce605a7c30c447d7ba7df49bb76274d27076d798
Oliver Grüttner <gruettner@protonmail.com>
2023-06-09 04:14:08 +0200

    feat(build): rome tool

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import compress from "astro-compress";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 
+import rome from "astro-rome";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://olis.tech',
   output: 'static',
-  integrations: [compress(), prefetch(), sitemap()],
+  integrations: [compress(), prefetch(), sitemap(), rome()]
 });
