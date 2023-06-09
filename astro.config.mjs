029d63a0a0640d9a69b144730fefc23587d0b2cb
Oliver Grüttner <gruettner@protonmail.com>
2023-06-09 03:57:46 +0200

    feat(build): astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
-
 import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://olis.tech',
   output: 'static',
-  integrations: [compress(), prefetch()]
+  integrations: [compress(), prefetch(), sitemap()],
 });
