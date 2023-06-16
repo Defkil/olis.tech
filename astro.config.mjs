802efc76503001ce319f93fcf39abe599704b5f9
Oliver Grüttner <gruettner@protonmail.com>
2023-06-16 18:47:13 +0200

    feat(build): rollup visualizer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import sitemap from '@astrojs/sitemap';
 import rome from 'astro-rome';
 import critters from 'astro-critters';
 import compressor from 'astro-compressor';
+import {visualizer} from "rollup-plugin-visualizer";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://olis.tech',
   output: 'static',
-  integrations: [sitemap(), rome(), critters(), compressor(), compress({css: false})]
+  integrations: [sitemap(), rome(), critters(), compressor(), compress({css: false})],
+  vite: {
+    plugins: [visualizer({filename: './dist/_astro/stats.html', gzipSize: true, brotliSize: true})]
+  }
 });
