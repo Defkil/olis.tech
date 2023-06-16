1b86618a98ac086b33f55bf01ed2ba2b394ab6e3
Oliver Grüttner <gruettner@protonmail.com>
2023-06-16 22:38:56 +0200

    feat(build): shared color variables between ts, css & scss

--- src/const/styles.ts
+++ src/const/styles.ts
@@ -1,12 +1,13 @@
+import style from './styles.module.scss';
 export const styles = {
-  "color-black": "#000",
-  "color-white": "#fff",
-  "color-primary": "#7Df9FF",
-  "color-secondary": "#2FFF2F",
-  "color-tertiary": "#FF00F5",
-  "color-quaternary": "#3300FF",
-  "color-quinary": "#FFFF00",
-  "color-senary": "#FF4911",
+  "color-black": style["color-black"],
+  "color-white": style["color-white"],
+  "color-primary": style["color-primary"],
+  "color-secondary": style["color-secondary"],
+  "color-tertiary": style["color-tertiary"],
+  "color-quaternary": style["color-quaternary"],
+  "color-quinary": style["color-quinary"],
+  "color-senary": style["color-senary"],
 };
 
 export const WaveSeparatorColors = [styles["color-secondary"], styles["color-quaternary"]];
