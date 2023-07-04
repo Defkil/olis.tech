8b728ffdafdd0cc9633de608ffb67eaade6612b8
Oliver Grüttner <gruettner@protonmail.com>
2023-07-04 13:35:43 +0200

    chore(build): moved site data to own file

---
+++ src/const/data.ts
@@ -0,0 +1,6 @@
+/*
+blog url, title, description, author-email
+ */
+export const SITE_TITLE = "Olis.tech";
+export const SITE_DESCRIPTION = "Olis Tech Blog";
+export const SITE_AUTHOR = "tech@olis.email";
