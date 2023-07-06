1c3b07ca681e873cbb4ca2f62f96836cd69d1b62
Oliver Grüttner <gruettner@protonmail.com>
2023-07-06 03:55:16 +0200

    pnpm(test): algolia search

---
+++ tests/layouts/components/AlgoliaSearch.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "@playwright/test";
+
+test("open algolia search with keyboard STRG+K", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.keyboard.press("Control+K");
+  const modal = await page.locator(".DocSearch.DocSearch-Container");
+  await expect(modal).toBeVisible();
+});
+
+test("algolia search entry should be invisible", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  const modal = await page.locator(".docsearch-hidden");
+  await expect(modal).not.toBeVisible();
+});
