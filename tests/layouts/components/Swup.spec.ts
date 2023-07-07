4b61f8337c0d5ae5a50cb19904c3c8ecd8a2977f
Oliver Grüttner <gruettner@protonmail.com>
2023-07-07 04:07:15 +0200

    feat(test): swup content change

--- tests/layouts/components/Swup.spec.ts
+++ tests/layouts/components/Swup.spec.ts
@@ -1,5 +1,13 @@
 import { expect, test } from "@playwright/test";
 
+test("swup will change the content at #swup", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.click("text=Impressum");
+  await page.waitForTimeout(1000);
+  const content = await page.$eval("#swup", (el) => el.textContent);
+  expect(content).toContain("Impressum");
+});
+
 test("swup will scroll down after changing the page", async ({ page }) => {
   await page.goto("http://localhost:3000/");
   await page.click("text=Impressum");
