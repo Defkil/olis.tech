beb7e3ab081273fe3a3da5dab3f7cd8c8e49da4e
Oliver Grüttner <gruettner@protonmail.com>
2023-07-23 19:43:14 +0200

    feat(build): macy & swup types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,30 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-declare module "macy";
+
+declare module "macy" {
+  /**
+   * masonry layout library
+   * https://github.com/bigbite/macy.js
+   * @param options
+   */
+  export default function macy(options: any): {
+    /**
+     * when called this recalculates the entire layout
+     * @param refresh  if true, will recalculate all items
+     */
+    recalculate: (refresh?: boolean) => void;
+    /**
+     * used to do something each time and image loads or after all images have been loaded
+     * @param cb  Function to run on image load
+     * @param onEveryImage If true it will run everytime an image loads
+     */
+    runOnImageLoad: (cb: () => void, onEveryImage?: boolean) => void;
+  };
+}
+declare module "@swup/head-plugin";
+declare module "@swup/slide-theme";
+declare module "@swup/preload-plugin";
+
+interface Window {
+  swup: import("swup").default;
+}
