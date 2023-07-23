efea0eb470074eddac139d213baf4bb37544f165
Oliver Grüttner <gruettner@protonmail.com>
2023-07-23 19:45:34 +0200

    refactor(build): swup to own file

---
+++ src/lib/swup.ts
@@ -0,0 +1,24 @@
+import Swup from "swup";
+import SwupHeadPlugin from "@swup/head-plugin";
+import SwupSlideTheme from "@swup/slide-theme";
+import SwupPreloadPlugin from "@swup/preload-plugin";
+
+const SWUP_CONTAINER_ID = "swup";
+const SWUP_PLUGINS = [new SwupPreloadPlugin(), new SwupHeadPlugin(), new SwupSlideTheme({ reversed: false })];
+
+/**
+ * Setup swup with plugins and assign it to window.swup
+ * called from layout
+ */
+export function setupSwup(): Swup {
+  const swup = new Swup({
+    plugins: SWUP_PLUGINS,
+  });
+  window.swup = swup;
+  return swup;
+}
+
+/** Scroll to the top of the swup container */
+export function scrollToSwupContent(): void {
+  document.getElementById(SWUP_CONTAINER_ID)?.scrollIntoView({ behavior: "smooth" });
+}
