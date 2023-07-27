7229a9867294f2bb6ace2f4c7a641d21965dd686
Oliver Grüttner <gruettner@protonmail.com>
2023-07-27 14:41:00 +0200

    refactor(test): changed redundant assertion

--- tests/layouts/components/PreloadSvg.spec.ts
+++ tests/layouts/components/PreloadSvg.spec.ts
@@ -2,11 +2,11 @@ import { expect, test } from "@playwright/test";
 
 test("svg with height=0,width=0 with patern and symbol should exist", async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  const preloadSvg = await page.$("svg");
+  const preloadSvg = (await page.$("svg"))!;
   expect(preloadSvg).toBeTruthy();
-  const preloadSvgHeight = await preloadSvg!.getAttribute("height");
+  const preloadSvgHeight = await preloadSvg.getAttribute("height");
   expect(preloadSvgHeight).toBe("0");
-  const preloadSvgWidth = await preloadSvg!.getAttribute("width");
+  const preloadSvgWidth = await preloadSvg.getAttribute("width");
   expect(preloadSvgWidth).toBe("0");
   const preloadSvgPatern = await page.$("svg > pattern");
   expect(preloadSvgPatern).toBeTruthy();
