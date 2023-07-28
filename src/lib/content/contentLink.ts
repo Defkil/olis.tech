6f91de4e9b079a86841bde6b0e68f411105db3b7
Oliver Grüttner <gruettner@protonmail.com>
2023-07-28 23:10:08 +0200

    feat(build): functions to resolve links

---
+++ src/lib/content/contentLink.ts
@@ -0,0 +1,16 @@
+/**
+ * Link to a category collection
+ * @param categoryCollection Category collection name
+ */
+export function contentLinkCategory(categoryCollection: string): string {
+  return "/" + categoryCollection;
+}
+
+/**
+ * Link to a post
+ * @param categoryCollection Category collection name
+ * @param slug Post slug
+ */
+export function contentLinkPost(categoryCollection: string, slug: string): string {
+  return "/" + categoryCollection + "/" + slug;
+}
