ec248a28ddca6d69c91dbf0bfeaa769ea90306dd
Oliver Grüttner <gruettner@protonmail.com>
2023-07-28 23:11:05 +0200

    feat(build): get last publish or update of a collection

---
+++ src/lib/helper/helperLastDate.ts
@@ -0,0 +1,21 @@
+/**
+ * Get the last publish or update date from a list of posts
+ * @param posts List of posts
+ */
+export function helperLastPublishOrUpdate(
+  posts: {
+    publishDate: Date;
+    updateDate?: Date;
+  }[],
+): Date {
+  let lastDate = new Date(0);
+  for (const post of posts) {
+    if (post.publishDate > lastDate) {
+      lastDate = post.publishDate;
+    }
+    if (post.updateDate && post.updateDate > lastDate) {
+      lastDate = post.updateDate;
+    }
+  }
+  return lastDate;
+}
