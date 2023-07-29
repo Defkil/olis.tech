002b7981b6a3a89552dc47b10fa84075178f0c63
Oliver Grüttner <gruettner@protonmail.com>
2023-07-30 01:47:46 +0200

    refactor(build): changed post schema: image not optional and add imageAlt

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,21 @@
-import { z, defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import type { CollectionConfig } from "astro/dist/content/utils";
 import { categories } from "../const/categories";
 
-const collectionSchemaDefault = z.object({
+/** Post schema for astro */
+export const postSchemaDefault = z.object({
   title: z.string(),
   description: z.string(),
-  image: z.string().url().optional(),
+  image: z.string().url(),
+  imageAlt: z.string(),
   publishDate: z.date(),
   updateDate: z.date().optional(),
 });
 
+/**
+ * Astro collections
+ * https://docs.astro.build/de/guides/content-collections/
+ */
 export const collections: {
   [key: string]: CollectionConfig;
 } = {};
@@ -17,6 +23,6 @@ export const collections: {
 for (const category of categories) {
   collections[category.collection] = defineCollection({
     type: "content",
-    schema: collectionSchemaDefault,
+    schema: postSchemaDefault,
   });
 }
