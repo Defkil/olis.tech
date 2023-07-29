a2a1ddab3181e115d4ec2dbf5bd9c08d6a66c9c9
Oliver Grüttner <gruettner@protonmail.com>
2023-07-30 01:46:56 +0200

    refactor(build): removed image

--- src/const/categories.ts
+++ src/const/categories.ts
@@ -1,46 +1,39 @@
-import type { CategoryData } from "../content/_categories";
+import type { CategoryData } from "../env";
 
 export const categories: CategoryData[] = [
   {
     title: "Allgemein",
     collection: "allgemein",
     description: "Allgemeine Informationen",
-    image: "https://via.placeholder.com/150",
   },
   {
     title: "Meine Projekte",
     collection: "projekte",
     description: "Meine Projekte",
-    image: "https://via.placeholder.com/150",
   },
   {
     title: "Webentwicklung",
     collection: "webentwicklung",
     description: "Webentwicklung Informationen",
-    image: "https://via.placeholder.com/150",
   },
   {
     title: "Programmierung",
     collection: "programmierung",
     description: "Programmierung Informationen",
-    image: "https://via.placeholder.com/150",
   },
   {
     title: "Tools",
     collection: "tools",
     description: "Tools Informationen",
-    image: "https://via.placeholder.com/150",
   },
   {
     title: "Linux",
     collection: "linux",
     description: "Linux Informationen",
-    image: "https://via.placeholder.com/150",
   },
   {
     title: "Sonstiges",
     collection: "sonstiges",
     description: "Sonstiges",
-    image: "https://via.placeholder.com/150",
   },
 ];
