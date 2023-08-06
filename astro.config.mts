f9b7221940df918d79d4417ea718432c88a36515
Oliver Grüttner <gruettner@protonmail.com>
2023-08-06 02:50:44 +0200

    feat(build): swup astro integration

--- astro.config.mts
+++ astro.config.mts
@@ -6,12 +6,24 @@ import critters from "astro-critters";
 import compressor from "astro-compressor";
 import { visualizer } from "rollup-plugin-visualizer";
 import { SITE_LINK } from "./src/const/data";
+import swup from "@swup/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_LINK,
   output: "static",
-  integrations: [sitemap(), rome(), critters(), compressor(), compress({ css: false })],
+  integrations: [
+    swup({
+      theme: "slide" as any,
+      containers: ["#swup"],
+      smoothScrolling: false,
+    }),
+    sitemap(),
+    rome(),
+    critters(),
+    compressor(),
+    compress({ css: false }),
+  ],
   vite: {
     plugins: [visualizer({ filename: "./dist/_astro/stats.html", gzipSize: true, brotliSize: true }) as any],
     build: {
--- src/lib/tools/swup.ts
+++ src/lib/tools/swup.ts
@@ -1,27 +1,12 @@
-import Swup from "swup";
-import SwupHeadPlugin from "@swup/head-plugin";
-import SwupSlideTheme from "@swup/slide-theme";
-import SwupPreloadPlugin from "@swup/preload-plugin";
-
 const SWUP_CONTAINER_ID = "swup";
-const SWUP_PLUGINS = [new SwupPreloadPlugin(), new SwupHeadPlugin(), new SwupSlideTheme({ reversed: false })];
-
 /**
  * Setup swup with plugins and assign it to window.swup
  * called from layout
  */
-export function setupSwup(): Swup {
-  const swup = new Swup({
-    plugins: SWUP_PLUGINS,
+export function setupSwup(): void {
+  document.addEventListener("swup:visit:end", () => {
+    document.getElementById(SWUP_CONTAINER_ID)?.scrollIntoView();
   });
-  window.swup = swup;
-  swup.on("contentReplaced", scrollToSwupContent);
-  return swup;
-}
-
-/** Scroll to the top of the swup container */
-function scrollToSwupContent(): void {
-  document.getElementById(SWUP_CONTAINER_ID)?.scrollIntoView();
 }
 
 /**
