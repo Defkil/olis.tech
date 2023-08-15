d53e238c9aaf61ad2fbab311bff24ac39a7dd573
Oliver Grüttner <gruettner@protonmail.com>
2023-08-15 20:32:49 +0200

    chore(build): removed comment

--- src/lib/helper/debounce.ts
+++ src/lib/helper/debounce.ts
@@ -19,22 +19,3 @@ export function debounce(func: () => void, wait: number) {
     timeout = setTimeout(later, wait);
   };
 }
-/*
-var totalRows = 4,
-itemCol = 0;
-for(var rowCount = 0; rowCount < totalRows; rowCount++){
-let newCol;
-newCol = document.createElement('div');
-newCol.className = 'col';
-document.getElementsByClassName('wrapper')[0].appendChild(newCol);
-}
-
-for(var itemCount = 0; itemCount < document.getElementsByClassName('window').length; itemCount++){
-document.getElementsByClassName('col')[itemCol].appendChild(document.getElementsByClassName('window')[0]);
-if(itemCol < totalRows - 1){
-  itemCol++;
-} else {
-  itemCol = 0;
-}
-}
-*/
