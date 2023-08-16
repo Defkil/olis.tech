67af5210737e925d35de30c7e827e329e5f82b19
Oliver Grüttner <gruettner@protonmail.com>
2023-08-16 18:21:39 +0200

    fix(test): #swup selector was replaced with main in swup3

--- tests/layouts/components/Swup.spec.ts
+++ tests/layouts/components/Swup.spec.ts
@@ -4,7 +4,7 @@ test("swup will change the content at #swup", async ({ page }) => {
   await page.goto("http://localhost:3000/");
   await page.click("text=Impressum");
   await page.waitForTimeout(1000);
-  const content = await page.$eval("#swup", (el) => el.textContent);
+  const content = await page.$eval("main", (el) => el.textContent);
   expect(content).toContain("Impressum");
 });
 
