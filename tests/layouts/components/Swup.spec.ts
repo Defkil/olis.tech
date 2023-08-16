4ef46aaa3f27a3b7dbe6f87541ab660ed8d31cda
Oliver Grüttner <gruettner@protonmail.com>
2023-08-16 20:06:10 +0200

    fix(build): removed scroll check, didn't work

--- tests/layouts/components/Swup.spec.ts
+++ tests/layouts/components/Swup.spec.ts
@@ -7,11 +7,3 @@ test("swup will change the content at #swup", async ({ page }) => {
   const content = await page.$eval("main", (el) => el.textContent);
   expect(content).toContain("Impressum");
 });
-
-test("swup will scroll down after changing the page", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await page.evaluate(() => window.scrollTo(0, 0));
-  await page.click(".js--posts>a:first-child");
-  await page.waitForTimeout(1000);
-  await expect(page.locator("main")).toBeInViewport();
-});
