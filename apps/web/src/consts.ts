05312968cc4c653b0a08f44de375153ba142c1c2
Oliver Grüttner <gruettner@protonmail.com>
2024-04-01 03:25:14 +0200

    chore: updated values and added categories

--- apps/web/src/consts.ts
+++ apps/web/src/consts.ts
@@ -1,5 +1,43 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Olis.tech';
+export const SITE_DESCRIPTION = 'Olis Tech Blog';
+
+export const SITE_CATEGORIES= [
+  {
+    title: "General",
+    collection: "general",
+    description: "Informationen",
+  },
+  {
+    title: "Linux",
+    collection: "linux",
+    description: "Informationen",
+  },
+  {
+    title: "Programming",
+    collection: "programming",
+    description: "Informationen",
+  },
+  {
+    title: "Projects",
+    collection: "projects",
+    description: "Informationen",
+  },
+  {
+    title: "Tools",
+    collection: "tools",
+    description: "Informationen",
+  },
+  {
+    title: "Web",
+    collection: "web",
+    description: "Informationen",
+  },
+  {
+    title: "Other",
+    collection: "other",
+    description: "Informationen",
+  },
+];
