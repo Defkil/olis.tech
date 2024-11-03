f60773420d60fb4daff9e24bf6457dcbea0a1180
Oliver Grüttner <gruettner@protonmail.com>
2024-11-03 14:39:41 +0100

    feat: Update site constants with new values

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Olis.tech";
+export const SITE_DESCRIPTION = "Olis Tech Blog";
+export const SITE_AUTHOR_MAIL = "tech@olis.email";
+export const SITE_LINK = "https://olis.tech/";
