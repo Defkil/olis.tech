650ebdc22c17ccfe1fef4d848393b888c3633caf
Oliver Grüttner <gruettner@protonmail.com>
2025-01-12 07:42:38 +0100

    feat: added old types with deprecated markings

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,33 @@
 /// <reference path="../.astro/types.d.ts" />
+// todo remove types here
+/** @deprecated */
+export interface CategoryData<T extends PostSchema = PostSchema> {
+  title: string;
+  collection: string;
+  description: string;
+  /** Custom category data */
+  custom?: {
+    layout: string;
+    schema: T;
+  };
+}
+
+/** @deprecated */
+export interface PostWindowProps {
+  category: string;
+  categoryLink: string;
+  lastPublishOrUpdate: string;
+  image: string;
+  imageAlt: string;
+  title: string;
+  excerpt: string;
+  link: string;
+}
+
+/** @deprecated */
+export const postSchema = postSchemaDefault.extend({
+  slug: z.string(),
+});
+
+/** @deprecated */
+export type PostSchema = z.infer<typeof postSchema>;
