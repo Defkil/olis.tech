41598dfec93cae72e2a7bb6471559aab92be8dc5
Oliver Grüttner <gruettner@protonmail.com>
2025-05-31 16:45:58 +0200

    chore: add category to astro schema

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,30 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'Olis.tech';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const BLOG_CATEGORIES: {
+  id: string;
+  name: string;
+}[] = [
+  {
+    id: 'projects',
+    name: 'Projects',
+  },
+  {
+    id: 'web-development',
+    name: 'Web Development',
+  },
+  {
+    id: 'programming',
+    name: 'Programming',
+  },
+  {
+    id: 'tools',
+    name: 'Tools',
+  },
+  {
+    id: 'other',
+    name: 'Other',
+  },
+]
+
+export const BLOG_CATEGORIES_IDS = BLOG_CATEGORIES.map((category) => category.id) as [string, ...string[]];
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,16 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import {glob} from 'astro/loaders';
+import {defineCollection, z} from 'astro:content';
+import {BLOG_CATEGORIES_IDS} from "./consts.ts";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		category: z.enum(BLOG_CATEGORIES_IDS),
 	}),
 });
 
