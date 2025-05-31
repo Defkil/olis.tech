0192fd02e2b40aeeecbbd3c23178bd2dbf5b0e72
Oliver Grüttner <gruettner@protonmail.com>
2025-05-31 17:51:30 +0200

    feat: site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
+export const STARTED_PROGRAMMING_IN_YEARS = new Date().getFullYear() - 2009;
 export const SITE_TITLE = 'Olis.tech';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'I\'m Oli, a passionate developer with ' + STARTED_PROGRAMMING_IN_YEARS + '+ years of experience exploring the digital world. This blog shares insights on web development, software engineering and hardware tinkering.';
 
 export const BLOG_CATEGORIES: {
   id: string;
